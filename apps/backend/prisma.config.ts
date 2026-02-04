4cbf3885eb1ac89785bd0b127660039102127edc
aleduque75 <alexandre.duque@gmail.com>
2026-02-04 08:14:19 -0300

    feat: Pare de rastrear arquivos de configuração locais e scripts

--- apps/backend/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-// apps/backend/prisma.config.ts
-import { defineConfig } from '@prisma/config'
-
-export default defineConfig({
-  datasource: {
-    // Aqui é onde a mágica da conexão acontece agora
-    url: process.env.DATABASE_URL,
-  },
-})
